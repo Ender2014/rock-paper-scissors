f134b63c4a4f38807f336aa4aa586853004586b8
Ender2014 <lau.zihao@gmail.com>
2024-10-04 20:53:11 +0800

    Removes uneeded console logs

--- scripts.js
+++ scripts.js
@@ -4,13 +4,14 @@ function getComputerChoice(){
 
     rand = Math.random() 
     if(rand < 0.33 ){
-        console.log(rand)
         return "scissors"
     } else if(rand < 0.66 ){
-        console.log(rand)
         return "rock"
     } else {
-        console.log(rand)
         return "paper"
     }
+}
+
+function getComputerChoice{
+
 }
