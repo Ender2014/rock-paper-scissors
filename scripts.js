72295c81947ec78c11732d117d62892b0f7a835d
Ender2014 <lau.zihao@gmail.com>
2024-10-04 21:34:21 +0800

    Added playRound(humanChoice, compChoice) function.
    This function plays a round of rock paper scissors. It increments
    humanScore or computerScore according to who wins.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
+// A function that randomly generates "rock", "paper", or "scissors" for comp.
 function getComputerChoice(){
+    // Genrate random number between 0 and 1.
     rand = Math.random() 
+    // Returns rock paper or scissors based based on the range of the random number.
     if(rand < 0.33 ){
         return "scissors"
     } else if(rand < 0.66 ){
@@ -9,17 +12,31 @@ function getComputerChoice(){
     }
 }
 
+// A function that prompts user to enter thier choice of "rock", "paper" or "scissors".
 function getHumanChoice(){
     return prompt("Enter your choice")
 }
 
+// Declare var to track human and comp scores (amount of wins).
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-    
-  }
-  
+// Function that plays a round of RCS. Logs results in console.
+function playRound(humanChoice ="rock", computerChoice="rock") {
+    if(humanChoice.toLowerCase() === computerChoice.toLowerCase()){
+        console.log(`Tie! ${computerChoice} is the same as ${humanChoice}!`)
+    }
+    else if((humanChoice.toLowerCase() === "rock" && computerChoice.toLowerCase() === "paper")
+        || (humanChoice.toLowerCase() === "scissors" && computerChoice.toLowerCase() === "rock")
+        || (humanChoice.toLowerCase() === "paper" && computerChoice.toLowerCase() === "scissors")
+    ){
+        console.log(`You Lose! ${computerChoice} beats ${humanChoice}!`)
+        computerScore++
+    } else{
+        console.log(`You Win! ${humanChoice} beats ${computerChoice}!`)
+        humanScore++
+    }
+}
   const humanSelection = getHumanChoice();
   const computerSelection = getComputerChoice();
   
